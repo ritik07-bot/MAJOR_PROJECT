9eb6d9f336dfdfd870ddc2d1c67bbeb82e49c47d
Ritik Roushan <ritikroushan777@gmail.com>
2026-07-01 18:39:58 +0530

    handle error

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app=express();
 const mongoose= require("mongoose");
 const Listing=require("./models/listing.js");
+const initDB = require("./init/index.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema}= require("./schema.js");
 
 const MONGO_URl="mongodb://127.0.0.1:27017/wanderlusts";
 
@@ -18,76 +22,82 @@ main().then(()=>{
 async function main(){
     await mongoose.connect(MONGO_URl);
 } 
-
+app.use(express.json());
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("Hi,i am root");
 })
 
-// app.get("/testListing",async(req,res)=>{
-//     let sampleListing= new Listing({
-//         title:"My new villa",
-//         description:"By the beach",
-//         price:1200,
-//         location:"Calangute,Goa",
-//         country:"India",
-//     });
+const validateListing=(req,res,next)=>{
+let {error} = listingSchema.validate(req.body);
+if(error){
+    let errorMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errorMsg);
+}else{
+    next();
+}
+}
 
-//     await sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("Successful testing");
-// })
-//Index route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index",{allListings});
-})
+}));
 
 //new route
-app.get("/listings/new",async(req,res)=>{
+app.get("/listings/new",wrapAsync(async(req,res)=>{
     res.render("listings/new");
-})
+}));
 
 //create new route
-app.post("/listings",async(req,res)=>{
-    let newListing = new Listing(req.body.listing);
+app.post("/listings",validateListing ,wrapAsync(async(req,res)=>{
+   
+   let newListing = new Listing(req.body.listing);
+    
     await newListing.save();
     res.redirect("/listings");
     
-})
+}));
 //edit route
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     let listing=await Listing.findById(id);
     res.render("listings/edit",{listing});
-})
+}));
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validateListing ,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const editListing=req.body.listing;
     await Listing.findByIdAndUpdate(id,{...editListing});
 
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //Delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}));
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}= req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show",{listing});
+}));
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page not found! ") )
 })
+app.use((err, req, res, next) => {
+    console.error(err);   // Terminal me original error print hoga
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("error.ejs",{message});
+});
 
 
 app.listen(8080,()=>{
--- models/listing.js
+++ models/listing.js
@@ -1,10 +1,10 @@
-;const mongoose=require("mongoose");
+const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
 const listingSchema = new Schema({
     title:{
         type:String,
-        required:true
+        required:true,
     },
     description:String,
     image: {
