c47213019e6fb6376522f4940e4b28938f122b8b
Ritik Roushan <ritikroushan777@gmail.com>
2026-07-13 16:31:45 +0530

    add authentication part

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const ExpressError = require("./utils/ExpressError.js");
 const cookieParser = require("cookie-parser");
 const session=require("express-session");
 const flash =require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
-const { appendFile } = require("fs/promises");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URl="mongodb://127.0.0.1:27017/wanderlusts";
 
@@ -40,7 +43,7 @@ const sessionOptions = {
     resave:false,
     saveUninitialized:true,
     cookie:{
-        exprires:Date.now()+7*24*60*60*1000,
+        expires:Date.now()+7*24*60*60*1000,
         maxAge:7*24*60*60*1000,
         httpOnly:true,
     }
@@ -53,35 +56,23 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
-// app.get("/signedCookies",(req,res)=>{
-//     res.cookie("made-in","India",{signed:true});
-//     res.send("check cookies");
-// });
-// app.get("/verify",(req,res)=>{
-//     console.log(req.signedCookies);
-//     res.send("verified");
-// });
-// app.get("/getCookies",(req,res)=>{
-//     res.cookie("greet","hello");
-//     res.send("give some cookies");
-// });
-// app.get("/greet",(req,res)=>{
-//     let {name="anynomous"} = req.cookies;
-//     res.send(`hii ${name}`);
-// });
-
-// app.all("*",(req,res,next)=>{
-//     next(new ExpressError(404,"Page not found! ") )
-// })
 app.all("*", (req, res, next) => {
     console.log("404 Route:", req.method, req.originalUrl);
     next(new ExpressError(404, "Page not found!"));
--- models/user.js
+++ models/user.js
@@ -1,15 +1,19 @@
-const { Email } = require("@mui/icons-material");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+
+const passportLocalMongoose = require("passport-local-mongoose").default;
+
+// console.log(passportLocalMongoose);
+// console.log(typeof passportLocalMongoose);
 
 const userSchema = new Schema({
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     }
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
